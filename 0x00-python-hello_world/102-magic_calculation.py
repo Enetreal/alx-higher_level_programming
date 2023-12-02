b381a11a8208f4f706b30adbdabfe307a3e8f6a1
Enetreal <wizzarddguru@gmail.com>
2023-12-02 15:18:03 -0800

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-import dis
-
 def magic_calculation(a, b):
-    return 98 + (a ** b)
-
-# Display the bytecode of the function
-dis.dis(magic_calculation)
+    return(98 + a ** b)
