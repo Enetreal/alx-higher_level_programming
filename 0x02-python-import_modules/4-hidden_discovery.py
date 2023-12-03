016bc23a6273b0272c33d88554b90ef6a51dc64c
Enetreal <wizzarddguru@gmail.com>
2023-12-03 15:23:51 -0800

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    for content in dir(sys):
+    import hidden_4
+    for content in dir(hidden_4):
         if content[:2] != "__":
             print(content)
