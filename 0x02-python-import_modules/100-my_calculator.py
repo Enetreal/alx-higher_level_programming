a84964dbe1f9c077c83fc965a0117c899c44bf9f
Enetreal <wizzarddguru@gmail.com>
2023-12-03 16:45:26 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,15 @@
-#!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
+# File: calculator_1.py
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+def add(a, b):
+    return a + b
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+def sub(a, b):
+    return a - b
 
-    result = None
+def mul(a, b):
+    return a * b
 
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    print(f"{a} {operator} {b} =", result)
+def div(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
