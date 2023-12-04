06ae2991a0001a56978523332c01b67cba4fc265
Enetreal <wizzarddguru@gmail.com>
2023-12-03 16:51:02 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,15 +1,36 @@
-# File: calculator_1.py
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-def add(a, b):
-    return a + b
+def main():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
 
-def sub(a, b):
-    return a - b
+    choice = input("Enter choice (1/2/3/4): ")
 
-def mul(a, b):
-    return a * b
+    if choice not in ('1', '2', '3', '4'):
+        print("Invalid choice. Please choose a valid operation.")
+        return
 
-def div(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+
+    if choice == '1':
+        result = add(a, b)
+    elif choice == '2':
+        result = sub(a, b)
+    elif choice == '3':
+        result = mul(a, b)
+    elif choice == '4':
+        try:
+            result = div(a, b)
+        except ValueError as e:
+            print(f"Error: {e}")
+            return
+
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
