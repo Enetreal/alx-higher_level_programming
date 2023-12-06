6c3cecd3582353b3f5b31d596e7ebdad90a1b088
Enetreal <wizzarddguru@gmail.com>
2023-12-06 00:10:27 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-
-    argc = len(argv) - 1
-    print("{} argument{}{}".format(argc, 's' if argc != 1 else '', ':' if argc else '.'))
-
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, arg))
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
