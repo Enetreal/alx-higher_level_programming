1dbb7ab1175117afa2c9140a185c3efd3497345c
Enetreal <wizzarddguru@gmail.com>
2023-12-06 00:17:24 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,36 +1,19 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-def main():
-    print("Select operation:")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-
-    choice = input("Enter choice (1/2/3/4): ")
-
-    if choice not in ('1', '2', '3', '4'):
-        print("Invalid choice. Please choose a valid operation.")
-        return
-
-    a = float(input("Enter first number: "))
-    b = float(input("Enter second number: "))
-
-    if choice == '1':
-        result = add(a, b)
-    elif choice == '2':
-        result = sub(a, b)
-    elif choice == '3':
-        result = mul(a, b)
-    elif choice == '4':
-        try:
-            result = div(a, b)
-        except ValueError as e:
-            print(f"Error: {e}")
-            return
-
-    print(f"Result: {result}")
 
 if __name__ == "__main__":
-    main()
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
