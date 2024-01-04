fadf7e432e340f0906f5ca2902a2e807705f7076
Enetreal <wizzarddguru@gmail.com>
2024-01-04 19:43:03 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,14 +2,20 @@
 
 """This module contains a function that multiplies two matrices"""
 
-import numpy as np
-
-
 def lazy_matrix_mul(m_a, m_b):
     """Return the multiplication of two matrices.
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
     """
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("Incompatible matrix dimensions for multiplication")
 
-    return (np.matmul(m_a, m_b))
+    result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                result[i][j] += m_a[i][k] * m_b[k][j]
+
+    return result
