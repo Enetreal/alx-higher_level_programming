ea69dfab6c6444e178cd085b1cbe6f941054bb30
Enetreal <wizzarddguru@gmail.com>
2024-01-05 01:55:39 +0100

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -7,55 +7,54 @@ of placing N non-attacking queens on an NxN chessboard.
 from sys import argv
 
 if __name__ == "__main__":
-    a = []
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
+
+    if not argv[1].isdigit():
         print("N must be a number")
         exit(1)
+
     n = int(argv[1])
+
     if n < 4:
         print("N must be at least 4")
         exit(1)
 
-    # initialize the answer list
-    for i in range(n):
-        a.append([i, None])
+    # Initialize the answer list
+    a = [[i, None] for i in range(n)]
 
     def already_exists(y):
-        """check that a queen does not already exist in that y value"""
-        for x in range(n):
-            if y == a[x][1]:
-                return True
-        return False
+        """Check that a queen does not already exist in that y value."""
+        return any(y == a[x][1] for x in range(n))
 
     def reject(x, y):
-        """determines whether or not to reject the solution"""
-        if (already_exists(y)):
+        """Determine whether or not to reject the solution."""
+        if already_exists(y):
             return False
+
         i = 0
-        while(i < x):
+        while i < x:
             if abs(a[i][1] - y) == abs(i - x):
                 return False
             i += 1
         return True
 
     def clear_a(x):
-        """clears the answers from the point of failure on"""
+        """Clear the answers from the point of failure on."""
         for i in range(x, n):
             a[i][1] = None
 
     def nqueens(x):
-        """recursive backtracking function to find the solution"""
+        """Recursive backtracking function to find the solution."""
         for y in range(n):
             clear_a(x)
             if reject(x, y):
                 a[x][1] = y
-                if (x == n - 1):  # accepts the solution
+                if x == n - 1:  # Accepts the solution
                     print(a)
                 else:
-                    nqueens(x + 1)  # moves on to next x value to continue
+                    nqueens(x + 1)  # Moves on to the next x value to continue
 
-    # start the recursive process at x = 0
+    # Start the recursive process at x = 0
     nqueens(0)
