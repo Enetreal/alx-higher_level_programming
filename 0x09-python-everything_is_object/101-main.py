016075a2411d5ec58de6d2b89675e2acd1a98fe5
Enetreal <wizzarddguru@gmail.com>
2024-01-06 05:50:08 +0100

    101-main

--- 0x09-python-everything_is_object/101-main.py
+++ 0x09-python-everything_is_object/101-main.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-LockedClass = __import__('101-locked_class').LockedClass
 
+class LockedClass:
+    """A locked class that only allows the creation of certain attributes."""
+    __slots__ = ['first_name', 'last_name']
+
+# Usage
 lc = LockedClass()
 lc.first_name = "John"
+
 try:
     lc.last_name = "Snow"
-except Exception as e:
+except AttributeError as e:
     print("[{}] {}".format(e.__class__.__name__, e))
