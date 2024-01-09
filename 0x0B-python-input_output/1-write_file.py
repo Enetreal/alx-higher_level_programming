34b1a7c2a1202e853cfefd8a4d4167655fd3d7f8
Enetreal <wizzarddguru@gmail.com>
2024-01-10 00:53:58 +0100

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A writing function"""
+
+def write_file(filename="", text=""):
+    try:
+        with open(filename, 'w', encoding='utf-8') as file:
+            num_characters_written = file.write(text)
+            return num_characters_written
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
