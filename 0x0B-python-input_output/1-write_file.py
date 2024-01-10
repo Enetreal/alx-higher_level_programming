9a1b54a48e2786ffa0a9cc7cbc7dd47d152eb1d2
Enetreal <wizzarddguru@gmail.com>
2024-01-10 01:19:22 +0100

    python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
-"""
-Defines a file-writing function.
-"""
+"""This module defines a file-writing function."""
+
 
 def write_file(filename="", text=""):
+    """Writes a string to a UTF8 text file
     """
-    Writes the specified text to the given filename.
-
-    Parameters:
-    - filename (str): The name of the file to write to.
-    - text (str): The text to be written to the file.
-
-    Returns:
-    int: The number of characters written to the file.
-    """
-    try:
-        with open(filename, "w", encoding="utf-8") as file:
-            num_characters_written = file.write(text)
-            return num_characters_written
-    except Exception as e:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 def append_write(filename="", text=""):
+    """Appends a string to the end of UTF8 text file"""
     try:
         with open(filename, 'a', encoding='utf-8') as file:
             num_characters_added = file.write(text)
             return num_characters_added
     except Exception as e:
+        # Handle the exception here if needed
         return 0
-
