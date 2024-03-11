f9ca5884c58e214567c298c470db85c1e0067dbc
Enetreal <wizzarddguru@gmail.com>
2024-03-11 16:04:06 +0100

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "JavaScript is amazing";
+const cFun = 'C is fun';
+const pyCool = 'Python is cool';
+const jAmaze = 'JavaScript is amazing';
 
-console.log(line1);
-console.log(line2);
-console.log(line3);
+console.log(cFun);
+console.log(pyCool);
+console.log(jAmaze);
