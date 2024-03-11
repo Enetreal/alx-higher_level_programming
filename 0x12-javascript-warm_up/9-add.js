91eb980f1a5ffdf71dc952e41a5ad2b43e9cc20e
Enetreal <wizzarddguru@gmail.com>
2024-03-12 00:51:35 +0100

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-// Define func 'add' that takes two param. 'a' and 'b' and returns the sum
-function add (a, b) {
+// Define a function 'add' that takes two parameters 'a' and 'b' and returns the sum
+function add(a, b) {
     return a + b;
   }
   
-  console.log(
-    add(Number.parseInt(process.argv[2]), Number.parseInt(process.argv[3]))
-  );
+  // Extract command line arguments and convert them to numbers
+  const arg1 = Number(process.argv[2]);
+  const arg2 = Number(process.argv[3]);
   
+  // Call the 'add' function with the provided arguments and log the result
+  console.log(add(arg1, arg2));
