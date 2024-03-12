0ed6ee71a0494ce1b04a51e314f61117a9ed036d
Enetreal <wizzarddguru@gmail.com>
2024-03-12 01:57:16 +0100

    12-objects.js

--- 0x12-javascript-warm_up/12-objects.js
+++ 0x12-javascript-warm_up/12-objects.js
@@ -3,6 +3,10 @@ const myObject = {
     type: 'object',
     value: 12
   };
+  
   console.log(myObject);
+  
+  // Update the value property to 89
   myObject.value = 89;
+  
   console.log(myObject);
