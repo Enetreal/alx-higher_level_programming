135df1f1140cc8c8af282e1a8dcd563bb2379f3c
Enetreal <wizzarddguru@gmail.com>
2024-03-13 02:16:35 +0100

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,39 +1,33 @@
 #!/usr/bin/node
-/**
- * Check the parameters provided
- */
+
 class Rectangle {
-    constructor (w, h) {
-      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let myVar = '';
-        let y = 0;
-        while (y < this.width) {
-          myVar += 'X';
-          y++;
-        }
-  
-        console.log(myVar);
-      }
-    }
-  
-    rotate () {
-      let temp = 0;
-      temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+  constructor (w, h) {
+    // Check if both width and height are greater than 0
+    if (w > 0 && h > 0) {
+      // If true, initialize the width and height attributes
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
-  
+
+  print () {
+    // Loop through each row
+    for (let i = 0; i < this.height; i++) {
+      // Print 'X' char to create a row
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    // Swap the values of width and height
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    // Double the values of width and height
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
