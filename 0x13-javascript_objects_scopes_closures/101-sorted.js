f77eb49937627553fe2f414b7e62095a652a9f1b
Enetreal <wizzarddguru@gmail.com>
2024-03-13 01:57:25 +0100

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,20 @@
 #!/usr/bin/node
+
 const dict = require('./101-data.js').dict;
 
 const newDict = {};
 
-Object.getOwnPropertyNames(dict).forEach(occurences => {
-  if (newDict[dict[occurences]] === undefined) {
-    newDict[dict[occurences]] = [occurences];
+// Iterate over the keys (user ids) and values occurrence
+Object.keys(dict).forEach(user => {
+  const occurrence = dict[user];
+
+  // If not a key, create a new key with an array containing userID
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [user];
   } else {
-    newDict[dict[occurences]].push(occurences);
+    // If already a key, push the user id to its array
+    newDict[occurrence].push(user);
   }
 });
+
 console.log(newDict);
