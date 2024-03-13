0d4fd165c84f9971453617fd171ea33bc85d149b
Enetreal <wizzarddguru@gmail.com>
2024-03-13 02:19:16 +0100

    3-rectangles.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
-/**
- * Check the parameters provided
- */
+
 class Rectangle {
-    constructor (w, h) {
-      if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let myVar = '';
-        let y = 0;
-        while (y < this.width) {
-          myVar += 'X';
-          y++;
-        }
-  
-        console.log(myVar);
-      }
+  constructor (w, h) {
+    // Check if both width and height are greater than 0
+    if (w > 0 && h > 0) {
+      // If true, initialize the width and height attributes
+      this.width = w;
+      this.height = h;
     }
   }
-  module.exports = Rectangle;
+
+  print () {
+    // Loop through each row
+    for (let i = 0; i < this.height; i++) {
+      // Print 'X' char to create a row
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
