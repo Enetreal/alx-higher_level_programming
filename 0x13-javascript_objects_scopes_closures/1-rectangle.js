a85f70ff6503745086cb45b3a0f53fb6d4f35c2f
Enetreal <wizzarddguru@gmail.com>
2024-03-13 02:24:42 +0100

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-/**
- * Represents a rectangle class with width and height attributes
- */
+
 class Rectangle {
-    constructor (w, h) {
-      this.width = w;
-      this.height = h;
-    }
+  // Constructor with two parameters w and h
+  constructor (w, h) {
+    // Initialize instance attribute width/height
+    this.width = w;
+    this.height = h;
   }
-  module.exports = Rectangle;
-  
+}
+
+module.exports = Rectangle;
