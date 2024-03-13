689df16ab7eb1fd2cd726259015dfb7585bc5620
Enetreal <wizzarddguru@gmail.com>
2024-03-13 02:35:11 +0100

    100-map.js8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const reversedList = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      reversedList.push(list[i]);
-    }
-  
-    return (reversedList);
-  };
-  
+  // Initialize an array
+  const reversedList = [];
+
+  // Iterate in reverse order
+  for (let u = list.length - 1; u >= 0; u--) {
+    // Add each element to the reversed list
+    reversedList.push(list[u]);
+  }
+
+  return reversedList;
+};
