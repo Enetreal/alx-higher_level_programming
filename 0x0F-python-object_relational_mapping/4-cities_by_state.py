2f29e517ccbcff3ed7992ba8f9cadcd21dc5e7db
Enetreal <wizzarddguru@gmail.com>
2024-03-16 23:23:59 +0100

    4-cities_by_states.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,21 +7,37 @@ the database `hbtn_0e_4_usa`.
 import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access the database and get the cities
-    from the database.
-    """
+def list_all_cities(username, password, database):
+    try:
+        # Connect to the database
+        db_connect = db.connect(host="localhost", port=3306,
+                                user=username, passwd=password, db=database)
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+        with db_connect.cursor() as db_cursor:
+            # Execute SQL query
+            db_cursor.execute("SELECT cities.id, cities.name, states.name \
                                 FROM cities JOIN states ON cities.state_id \
                                 = states.id ORDER BY cities.id ASC")
-        rows_selected = db_cursor.fetchall()
+            rows_selected = db_cursor.fetchall()
 
-    if rows_selected is not None:
-        for row in rows_selected:
-            print(row)
+        # Display the results
+        if rows_selected:
+            for row in rows_selected:
+                print(row)
+        else:
+            print("No cities found in the database.")
+
+    except db.Error as e:
+        print(f"Database error: {e}")
+    finally:
+        # Close the database connection
+        if 'db_connect' in locals():
+            db_connect.close()
+
+if __name__ == '__main__':
+    # Check if correct number of arguments is provided
+    if len(argv) != 4:
+        print("Usage: ./list_cities.py <username> <password> <database>")
+        exit(1)
+
+    list_all_cities(argv[1], argv[2], argv[3])
