a35164a0d27b103fc738e57250cb820c020734a3
Enetreal <wizzarddguru@gmail.com>
2024-03-17 07:16:11 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,17 +9,17 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3])
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
     cur = conn.cursor()
     state_name = sys.argv[4]
     cur.execute(
             'SELECT * FROM states WHERE CAST(name AS BINARY) LIKE ' +
             'CAST("{}" AS BINARY) ORDER BY id ASC;'.format(state_name)
-        )
+            )
     rows = cur.fetchall()
 
     for res in rows:
