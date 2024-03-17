85c5aa872a92fef7c37bc9e00a5c384a6e3364d3
Enetreal <wizzarddguru@gmail.com>
2024-03-17 07:14:34 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,10 +8,10 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3])
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
 
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states WHERE name
