9990baa8f9cbe6f00271078c8c304ea508fd8a8a
Enetreal <wizzarddguru@gmail.com>
2024-03-17 17:06:26 -0700

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
-"""Prints the State object with the given name"""
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
 
-import sys
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-
-def main():
+if __name__ == "__main__":
     """
-    Main function to query the database for the State object
-    with the given name and print its ID if found.
+    Access to the database and get a state
+    from the database.
     """
-    if len(sys.argv) == 5:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
 
-        DATABASE_URL = "mysql://{}:{}@localhost:3306/{}".format(
-            username, password, database
-        )
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3])
 
-        engine = create_engine(DATABASE_URL)
+    engine = create_engine(db_url)
+    Session = sessionmaker(bind=engine)
 
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    session = Session()
 
-        # Query db for state
-        state = session.query(State).filter(State.name == state_name).first()
-
-        if state:
-            print(state.id)
-        else:
-            print("Not found")
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
     else:
-        print("Usage: ./script.py <username> <password> <database>
-                <state_name>")
-        sys.exit(1)
-
-
-if __name__ == '__main__':
-    main()
+        print("Not found")
